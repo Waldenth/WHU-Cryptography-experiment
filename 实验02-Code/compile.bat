5cb330a6e28e83f949ab5ba7a00f3e60521949dc
Waldenth <waldenth@outlook.com>
2020-10-10 22:52:09 +0800

    add comiple helper

---
+++ 实验02-Code/compile.bat
@@ -0,0 +1,6 @@
+javac .\encrypt\keygenerate\GenerateKey.java -encoding UTF-8
+javac .\encrypt\permutation\*.java -encoding UTF-8 
+javac .\encrypt\DECRYPT.java -encoding UTF-8
+javac .\encrypt\ENCRYPT.java -encoding UTF-8
+javac Test.java -encoding UTF-8
+%java Test 执行编译文件,.java文件执行并不是UTF-8%
