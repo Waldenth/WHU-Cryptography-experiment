bc178897a8ecf2d2bda992262d1335e186f079da
Waldenth <waldenth@outlook.com>
2020-10-13 21:31:03 +0800

    add AES encrypt MixColumn function

--- 实验03-Code/AES/encrypt/RoundFunction.java
+++ 实验03-Code/AES/encrypt/RoundFunction.java
@@ -1,6 +1,7 @@
 package AES.encrypt;
 
 import AES.SBoxes;
+import AES.Xtime;
 
 public class RoundFunction {
     public static byte XOR(byte input1,byte input2){
@@ -30,7 +31,75 @@ public class RoundFunction {
     }
 
     public static byte[][] MixColumn(byte[][] input){
+        byte []temp=new byte[4];
+        
+        byte[][] mixMatrix={
+            {0x02,0x03,0x01,0x01},
+            {0x01,0x02,0x03,0x01},
+            {0x01,0x01,0x02,0x03},
+            {0x03,0x01,0x01,0x02}
+        };
+        
 
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                temp[i]=input[j][i];
+            }
+            temp=MatrixTimesAES(mixMatrix, temp);
+            for(int j=0;j<4;j++){
+                input[j][i]=temp[i];
+            }
+        }
         return input;
     }
+
+    public static byte GFtime(byte input1,byte input2){
+        byte ans=0;
+        int high=(input2>>>4)&(0b00001111);
+        int low=(input2)&(0b00001111);
+        switch (input1){
+            case 0x1:
+                ans=input2;
+                break;
+            case 0x2:
+                ans=Xtime.xtimeTable[high][low];
+                break;
+            case 0x3:
+                ans=(byte)(input2^Xtime.xtimeTable[high][low]);
+                break;
+            default:
+                System.out.println("Error: can not calcullate in GF(2^8)");
+                System.exit(-1);
+        }
+        return ans;
+    }
+
+    public static byte[] MatrixTimesAES(byte[][]matrix1,byte[]matrix2){
+        byte[]ans=new byte[4];
+        if(matrix1.length!=4||matrix1[0].length!=4||matrix2.length!=4){
+            System.out.println("Error, can not do Matrix times");
+            System.exit(-1);
+        }
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                ans[i]= (byte)(ans[i]^GFtime(matrix1[i][j], matrix2[j]));
+            }
+        }
+        return ans;
+    }
+
+    private static void Test(){
+        byte[][] mixMatrix={
+            {0x02,0x03,0x01,0x01},
+            {0x01,0x02,0x03,0x01},
+            {0x01,0x01,0x02,0x03},
+            {0x03,0x01,0x01,0x02}
+        };
+        byte[] inputData={(byte)0x4f,(byte)0xb1,(byte)0xe8,(byte)0x4f};
+        byte[] outputata=MatrixTimesAES(mixMatrix, inputData);
+        for(int i=0;i<4;i++){
+            System.out.printf("0x%x\n",outputata[i]);
+        }
+    }
+
 }
